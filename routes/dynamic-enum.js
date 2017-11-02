5b2662a5ffc54eb8f37e3f6efde8c508ed544c69
Rittmeyer Balz <balz.rittmeyer@nzz.ch>
2017-11-02 18:49:47 +0100

    update schema

---
+++ routes/dynamic-enum.js
@@ -0,0 +1,50 @@
+const Boom = require('boom');
+const Joi = require('joi');
+
+const maxMinimapInitialZoomOffset = -9;
+
+function getMinimapInitialZoomOffsetEnum(item) {
+  let initialZoomLevel = item.options.initialZoomLevel;
+
+  // check if initialZoomLevel is automatic, then we do not know the level and thus allow all offset options
+  if (initialZoomLevel === -1) {
+    initialZoomLevel = 18; // set it to the max zoom level available to allow for the maximum offset
+  }
+
+  const choices = [0];
+  const titles = ['automatisch'];
+  let i = -1;
+  while (Math.abs(i) < initialZoomLevel && i >= maxMinimapInitialZoomOffset) {
+    choices.push(i);
+    titles.push(i);
+    i--;
+  }
+
+  return {
+    enum: choices,
+    enum_titles: titles
+  };
+}
+
+module.exports = {
+  method: 'POST',
+  path: '/dynamic-enum/{optionName}',
+  config: {
+    validate: {
+      payload: Joi.object()
+    },
+    cors: true
+  },
+  handler: function(request, reply) {
+    if (request.params.optionName === 'availableParties') {
+      let parties = request.payload.parties.map(p => p.name);
+      let options = {
+        enum: parties,
+        enum_titles: parties
+      };
+      return reply(options).type('application/json');
+    }
+
+    return reply(Boom.badRequest());
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const schema = require('./schema.js')
 const stylesheet = require('./stylesheet.js')
 const htmlStatic = require('./rendering-info/html-static.js')
+const dynamicEnum = require('./dynamic-enum.js')
 
 module.exports = [
   schema,
   stylesheet,
-  htmlStatic
+  htmlStatic,
+  dynamicEnum
 ]
