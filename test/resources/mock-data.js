2773c9b4cc83cad92338d74df6f9dc8b475eae8a
Rittmeyer Balz <balz.rittmeyer@nzz.ch>
2017-11-07 17:37:58 +0100

    add dom tests

--- test/resources/mock-data.js
+++ test/resources/mock-data.js
@@ -1,34 +1,32 @@
 module.exports = {
-  title: 'nisi est',
-  data: {
-    no: {
-      parties: [
-        'pariatur sit sunt',
-        'ullamco'
-      ],
-      organisations: [
-        'laborum ve',
-        'ea quis exercitation officia'
-      ]
-    },
-    yes: {
-      parties: [
-        'ut',
-        ''
-      ],
-      organisations: [
-        '',
-      ]
-    },
-    undecided: {
-      parties: [
-        'deserunt elit esse quis occaecat',
-        'irure tempor minim eu',
-        'magna'
-      ],
-      organisations: [
-        'sed id Ut si'
-      ]
+  "toolRuntimeConfig": {
+    "displayOptions": {
+      "hideTitle": false
     }
-  }
+  },
+  "title": "Diese Koalitionen sind nach den Wahlen möglich",
+  "possibleCoalitions": [
+    ["ÖVP", "SPÖ"],
+    ["ÖVP", "FPÖ"],
+    ["SPÖ", "FPÖ"],
+    ["ÖVP"]
+  ],
+  "parties": [
+    {
+      "name": "ÖVP",
+      "seats": 62,
+      "color": "#0a0a0a"
+    },
+    {
+      "name": "SPÖ",
+      "seats": 52,
+      "color": "#c31906"
+    },
+    {
+      "name": "FPÖ",
+      "seats": 51,
+      "color": "#0084c7"
+    }
+  ],
+  "totalSeats": 183
 }
