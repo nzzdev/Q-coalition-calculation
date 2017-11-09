3cdff31b60d27892cd56e513e1ef5f42283bc79e
Rittmeyer Balz <balz.rittmeyer@nzz.ch>
2017-11-09 14:13:19 +0100

    add health route

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,11 @@ const schema = require('./schema.js')
 const stylesheet = require('./stylesheet.js')
 const htmlStatic = require('./rendering-info/html-static.js')
 const dynamicEnum = require('./dynamic-enum.js')
+const health = require('./health.js')
 
 module.exports = schema.concat([
   stylesheet,
   htmlStatic,
-  dynamicEnum
+  dynamicEnum,
+  health
 ])
