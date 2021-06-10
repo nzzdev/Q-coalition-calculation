d5795a65fe25f058ec33a0ea3b36bd8ec29874dc
Manuel Roth <manuelroth@hotmail.ch>
2021-06-10 15:20:47 +0200

    Migrate from dynamic-enum to dynamic-schema

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const schema = require("./schema.js");
 const stylesheet = require("./stylesheet.js");
 const htmlStatic = require("./rendering-info/html-static.js");
-const dynamicEnum = require("./dynamic-enum.js");
+const dynamicSchema = require("./dynamic-schema.js");
 const health = require("./health.js");
 const fixtures = require("./fixtures/data");
 const locales = require("./locales.js");
@@ -9,8 +9,8 @@ const locales = require("./locales.js");
 module.exports = schema.concat([
   stylesheet,
   htmlStatic,
-  dynamicEnum,
+  dynamicSchema,
   health,
   locales,
-  fixtures
+  fixtures,
 ]);
