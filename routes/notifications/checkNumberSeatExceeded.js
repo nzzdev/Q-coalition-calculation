2a5198a860cb74eb7d73eeddb9fee331214c3521
Franco Gervasi <franco.gervasi@nzz.ch>
2021-06-23 16:04:07 +0200

    Fix show notify when seats fields have no content

--- routes/notifications/checkNumberSeatExceeded.js
+++ routes/notifications/checkNumberSeatExceeded.js
@@ -16,14 +16,19 @@ module.exports = {
   handler: function (request, h) {
     try {
       const item = request.payload.item;
+      const totalSeats = request.payload.item.totalSeats;
       // removing the header row first
-      const totalSeats = item.parties.reduce(
+      const totalSeatsParties = item.parties.reduce(
         (a, b) =>
           (typeof a == "number" ? a : a.seats) +
           (typeof b == "number" ? b : b.seats)
       );
 
-      if (totalSeats <= request.payload.item.totalSeats) {
+      if (
+        !totalSeats ||
+        Number.isNaN(totalSeatsParties) ||
+        totalSeatsParties <= totalSeats
+      ) {
         return null;
       }
       return {
